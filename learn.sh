d9b64e06babf9c34bacdd3c3aa5e1563fe7f204b
shiva <ssibasissingh@gmail.com>
2023-04-15 18:18:15 +0530

    last commit

---
+++ learn.sh
@@ -0,0 +1,27 @@
+
+#!/bin/bash
+
+echo $BASH
+echo $BASH_VERSION
+echo $HOME
+echo $PWD
+name=shiva
+
+echo THe name is $name
+echo please enter your name 
+read name1
+echo Please Enter Yuor Emp Id
+read Emp_id
+echo you are $name1
+echo your Employ Id $Emp_id
+echo Welcome to our Company
+echo Enter names
+read name1 name2 name3
+echo Names : $name1 , $name2 , $name3
+read -p 'username : ' user_var
+
+
+read -sp 'password : ' pass_var
+echo "username : $user_var"
+echo "password : $pass_var"
+
